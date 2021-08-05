fe3b0d505812bcd6329c43c78c24163cab4bb776
Darleysantana <darleyhyuga@gmail.com>
2021-08-04 23:41:54 -0300

    Foi introduzido nas classes e em algumas variaveis o abstract e protected

--- src/one/digitalinnovation/digitalbank/TesteBanco/TesteGerente.kt
+++ src/one/digitalinnovation/digitalbank/TesteBanco/TesteGerente.kt
@@ -1,5 +1,7 @@
 package one.digitalinnovation.digitalbank
 
+import one.digitalinnovation.digitalbank.Analista
+
 fun main() {
     val Maria = Analista(salario = 5000.00, nome = "João Pedro", cpf = "123.123.123-11")
     imprimeRelatorio.imprime(Maria)
